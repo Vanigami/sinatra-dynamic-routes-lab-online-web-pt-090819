cec8bb695b718a7f65a3fcaead132c8d6cd7760d
Vanna Winland <vgwinland@gmail.com>
2019-12-10 22:55:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,12 +10,12 @@ class App < Sinatra::Base
     (params[:number].to_i **2).to_s
   end
 
-  get "/say/:number/:phrase" do 
+  get "/say/:number/:phrase" do
     answer = ''
-    
-    params[:number].to_i.times do 
+
+    params[:number].to_i.times do
       answer += params[:phrase]
     end
   end
-    
+
 end
