1f618665421cda01f2c5bfcc6e3054b550ec8802
Vanna Winland <vgwinland@gmail.com>
2019-12-10 23:16:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,34 +1,33 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-  get "/reversename/:name" do
+  get '/reversename/:name' do
     params[:name].reverse
   end
 
-  get "/square/:number" do
-    (params[:number].to_i **2).to_s
+  get '/square/:number' do
+    (params[:number].to_i ** 2).to_s
   end
 
-  get "/say/:number/:phrase" do
+  get '/say/:number/:phrase' do
     answer = ''
 
     params[:number].to_i.times do
       answer += params[:phrase]
-
     end
+
     answer
   end
 
-  get "/say/:word1/:word2/:word3/:word4/:word5" do
-    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[word5]}."
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
-  get "/:operation/:number1/:number2" do
+  get '/:operation/:number1/:number2' do
     number1 = params[:number1].to_i
     number2 = params[:number2].to_i
 
-    answer = "Unable to perform this operation"
+    answer = 'Unable to perform this operation'
 
     case params[:operation]
     when 'add'
