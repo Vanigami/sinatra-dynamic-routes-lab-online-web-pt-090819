5a31fc0d609ce54bd92ef02400ec1ef148fea060
Vanna Winland <vgwinland@gmail.com>
2019-12-10 23:07:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ class App < Sinatra::Base
     number1 = params[:number1].to_i
     number2 = params[:number2].to_i
 
-    answer = ""
+    answer = "nO"
 
     case params[:operation]
     when 'add'
